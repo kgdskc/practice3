31cc6119d69b9f13b00aad8edfdb5d6351c7839e
kgdskc <wajima0503@gmail.com>
2021-09-29 13:19:08 +0000

    [Practice]

--- hello.rb
+++ hello.rb
@@ -1,4 +1,39 @@
-puts 'Hello World!'
-puts 5+8
+# puts 'Hello World!'
+# puts 5+8
 
-puts '私の名前はメンター太郎です。年齢は' + 24.to_s + '歳です。'
+# puts '私の名前はメンター太郎です。年齢は' + 24.to_s + '歳です。'
+
+# a = 0
+# b = 0
+# c = 1
+# n = 0
+# while n < 47
+#   d = a + b + c
+#   a = b
+#   b = c
+#   c = d
+#   n += 1
+# end
+
+# puts c
+
+# a = 4
+# b = 3
+# c = a ^ 2 + b ^ 2
+# d = c ^ 2
+# n = d / 
+
+# a = 6
+# b = 8
+# c = a * a + b * b
+# n = c**(1/2.0)
+# puts n
+
+a = 6
+c = 10
+b = c * c - a * a
+n = b**(1/2.0)
+puts n
+# puts "WEBCAMPは".include?("い")
+
+# puts "webcamp".sum
